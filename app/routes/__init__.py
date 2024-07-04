b71912106e463488b631e76aa1345e9b5555f998
BlazzByte <nrwrnntemporal2@gmail.com>
2024-07-04 10:51:11 -0400

    Refactor: Remove unnecesary files

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from . import products, route2
+from . import products
 
 api_router = APIRouter()
 
-api_router.include_router(route2.router, prefix="/route2", tags=["api"])
 api_router.include_router(products.router, prefix="/products", tags=["api"])
 
 __all__ = ['api_router']
